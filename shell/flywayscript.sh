f09bc17aef8779d5795d3875859887b139b5969c
pradeepkumar <123password>
2019-04-28 01:47:13 +0530

    cleanup

--- shell/flywayscript.sh
+++ shell/flywayscript.sh
@@ -3,11 +3,11 @@
 ############ Author: Pradeepkumar C G ##########
 ################################################
 
-FILE="/home/pkumar/Documents/Momentum/Stories/FJCRTPF.csv"
+FILE="/home/pkumar/Documents/JIRA/Stories/Sample.csv"
 
 if [ -f "$FILE" ]; then
 	while IFS=',' read col1 col2 col3 col4 col5 col6 col7 col8; do
-		echo 'INSERT INTO FJCRTPF (ID, CLIENT_NAME, SHORT_NAME, STATUS, SCHEDULING, REPORT_TYPE, ESCALATION_TIME, ESCALATION_METHOD, PLATFORM) VALUES ((SELECT Max(ID)+1 From FJCRTPF)', \'$col1\',  \'$col2\', \'$col3\', \'$col4\', \'$col5\', $col6, \'$col7\', \'$col8\'');' >>/tmp/output.sql
+		echo 'INSERT INTO Sample (ID, CLIENT_NAME, SHORT_NAME, STATUS, SCHEDULING, REPORT_TYPE, ESCALATION_TIME, ESCALATION_METHOD, PLATFORM) VALUES ((SELECT Max(ID)+1 From FJCRTPF)', \'$col1\',  \'$col2\', \'$col3\', \'$col4\', \'$col5\', $col6, \'$col7\', \'$col8\'');' >>/tmp/output.sql
 	done < $FILE
 
 fi
